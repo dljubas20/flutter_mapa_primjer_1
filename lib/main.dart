e85b1814e6b6657da6cc212a2d1bfdadb4bd497a
Dario Ljubas <darioljubas33@gmail.com>
2022-11-19 19:55:23 +0100

    Promijenjen naslov i dodana navigacijska traka

--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Mapa Primjer 1',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Navigacijska traka")
+        ),
       ),
     );
   }
